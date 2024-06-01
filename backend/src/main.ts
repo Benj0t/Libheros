5e979b415e78cbf16ab6fdaecc00f6221fdfe70c
Benjot <bmoreau89000@gmail.com>
2024-06-01 17:46:19 +0200

    EDIT main.ts to use env variable = check for db health

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import checkEnv from './utils/check-env';
+import * as dotenv from 'dotenv';
+import Knex from 'knex';
+import config from './knex/knexfile';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    checkEnv();
+
+    const knex = Knex(config);
+
+    // Testing DB connection before starting listening server
+    await knex.raw('SELECT 1+1 as result');
+
+    const app = await NestFactory.create(AppModule);
+
+    const port = process.env.PORT as string;
+    await app.listen(port);
+  } catch (err) {
+    console.log('Error during application startup', { err });
+    process.exit(-1);
+  }
 }
 bootstrap();
