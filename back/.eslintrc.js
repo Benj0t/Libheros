1a75af9d62942455ec9f3c411a702cefc82a61e7
Benjot <bmoreau89000@gmail.com>
2024-06-01 15:59:58 +0200

    Init NestJS using nest cli. Also setup TS (with ESLint and Prettier)

---
+++ back/.eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:jsdoc/recommended',
+    'plugin:import/typescript',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+    es6: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'no-nested-ternary': 'error',
+    'import/no-unresolved': ['error'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: ['builtin', ['external', 'internal'], 'parent', 'sibling', 'index'],
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-useless-constructor': 'error',
+    'spaced-comment': ['error', 'always', { markers: ['/'] }],
+    'no-else-return': ['error', { allowElseIf: false }],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: 'var', next: 'return' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+    ],
+  },
+};
